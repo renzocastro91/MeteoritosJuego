bf9467eb733dca2251caaeff4b4eb47136a1f1b9
renzocastro91 <renzogustavocastro99@gmail.com>
2023-10-23 11:51:38 -0300

    Agregado Movimiento Player
    Agregado:
     - Script de Player
     - Movimientos y Rotación Player

---
+++ Juego/Naves/Player/Player.gd
@@ -0,0 +1,36 @@
+class_name Player
+extends RigidBody2D
+
+#Atributos Export
+export var potencia_motor:int = 20
+export var portencia_rotacion:int = 280
+
+#Atributos
+var empuje:Vector2 = Vector2.ZERO
+var dir_rotacion:int = 0
+
+#Métodos
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	apply_central_impulse(empuje.rotated(rotation))
+	apply_torque_impulse(dir_rotacion * portencia_rotacion)
+	
+
+func _process(delta: float) -> void:
+	player_input()
+	
+#Métodos Custom
+func player_input() -> void:
+	#Empuje
+	empuje = Vector2.ZERO
+	if Input.is_action_pressed("mover_adelante"):
+		empuje = Vector2(potencia_motor,0)
+	elif Input.is_action_pressed("mover_atras"):
+		empuje = Vector2(-potencia_motor, 0)
+		
+	#Rotación
+	dir_rotacion = 0
+	if Input.is_action_pressed("rotar_antihorario"):
+		dir_rotacion -= 1
+	elif Input.is_action_pressed("rotar_horario"):
+		dir_rotacion += 1 
+	
