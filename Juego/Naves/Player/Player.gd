42346903a6260444a04cc1aedcb9ddc61aeeae98
renzocastro91 <renzogustavocastro99@gmail.com>
2023-10-27 21:01:50 -0300

    Agregado SFX Motor
    Agregado:
    - SFX Motor
    - Atenuación del sonido de aceleración y desaceleración

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -4,6 +4,7 @@ extends RigidBody2D
 #Atributos Export
 export var potencia_motor:int = 20
 export var portencia_rotacion:int = 280
+export var estela_maxima:int = 150
 
 #Atributos
 var empuje:Vector2 = Vector2.ZERO
@@ -12,6 +13,8 @@ var dir_rotacion:int = 0
 #Atributos onready
 onready var canion:Canion = $Canion
 onready var laser:RayoLaser = $LaserBeam2D
+onready var estela:Estela = $EstelaPuntoInicio/Trail2D
+onready var motor_sfx:Motor = $MotorSFX
 
 #Métodos
 func _unhandled_input(event: InputEvent) -> void:
@@ -21,7 +24,16 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 	if event.is_action_released("disparo_secundario"):
 		laser.set_is_casting(false)
-		
+	
+	#Control estela y sonido del motor
+	if event.is_action_pressed("mover_adelante"):
+		estela.set_max_points(estela_maxima)
+		motor_sfx.sonido_on()
+	elif event.is_action_pressed("mover_atras"):
+		estela.set_max_points(0)
+		motor_sfx.sonido_off()
+	
+	
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * portencia_rotacion)
