d2884bf9e9555ddb4f54c8e1b3891eb8cc12fca3
renzocastro91 <renzogustavocastro99@gmail.com>
2023-10-27 22:00:52 -0300

    Agregado Explosión
    Agregado:
    - Explosión animación
    - Explosión SFX

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -69,6 +69,7 @@ func controlador_estados(nuevo_estado: int) -> void:
 		ESTADO.MUERTO:
 			colisionador.set_deferred("disabled",true)
 			canion.set_puede_disparar(true)
+			Eventos.emit_signal("nave_destruida", global_position, 3)
 			queue_free()
 		_:
 			printerr("Error de estado")
@@ -101,6 +102,9 @@ func player_input() -> void:
 		canion.set_esta_disparando(true)
 	if Input.is_action_just_released("disparo_principal"):
 		canion.set_esta_disparando(false)
+
+func destruir() -> void:
+	controlador_estados(ESTADO.MUERTO)
 
 #Señales internas
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
--- Juego/Niveles/Nivel.gd
+++ Juego/Niveles/Nivel.gd
@@ -4,6 +4,9 @@ extends Node2D
 #Atributos onready
 onready var contenedor_proyectiles:Node
 
+#Atributos export 
+export var explosion:PackedScene = null
+
 #Métodos
 func _ready() -> void:
 	conectar_seniales()
@@ -12,11 +15,20 @@ func _ready() -> void:
 #Métodos Custom 
 func conectar_seniales() -> void:
 	Eventos.connect("disparo",self,"_on_disparo")
+	Eventos.connect("nave_destruida",self,"_on_nave_destruida")
 	
 func crear_contenedores() -> void:
 	contenedor_proyectiles = Node.new()
 	contenedor_proyectiles.name = "ContendorProyectiles"
 	add_child(contenedor_proyectiles)
+	
+func _on_nave_destruida(posicion: Vector2, num_explosiones: int) -> void:
+	for i in range(num_explosiones):
+		var new_explosion:Node2D = explosion.instance()
+		new_explosion.global_position = posicion
+		add_child(new_explosion)
+		yield(get_tree().create_timer(0.6),"timeout")
+		
 	
 func _on_disparo(proyectil:Proyectil) -> void:
 	contenedor_proyectiles.add_child(proyectil)
