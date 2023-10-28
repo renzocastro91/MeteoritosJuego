98475b8a482a90e9b0be3e324ea68bf5c72d881a
renzocastro91 <renzogustavocastro99@gmail.com>
2023-10-28 11:40:31 -0300

    Agregado Daño
    Agregado:
    - Daño disparo principal
    - Daño disparo secundario
    - Daño del enemigo a Player

--- Juego/Proyectiles/Proyectil.gd
+++ Juego/Proyectiles/Proyectil.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 #Atributos
 var velocidad:Vector2 = Vector2.ZERO
-var danio:float
+var danio:float = 1.0
 
 #Métodos
 func _physics_process(delta: float) -> void:
@@ -15,5 +15,20 @@ func crear(pos: Vector2, dir: float, vel:float, danio_p:int) -> void:
 	velocidad = Vector2(vel,0).rotated(dir)
 
 
+#Métodos custom
+func daniar(otro_cuerpo: CollisionObject2D) -> void:
+	if otro_cuerpo.has_method("recibir_danio"):
+		otro_cuerpo.recibir_danio(danio)
+	queue_free()
+	
+#Señales internas	
+
 func _on_VisibilityNotifier2D_screen_exited() -> void:
 	queue_free()
+
+
+func _on_Proyectil_area_entered(area: Area2D) -> void:
+	daniar(area)
+	
+func _on_Proyectil_body_entered(body: Node) -> void:
+	daniar(body)
